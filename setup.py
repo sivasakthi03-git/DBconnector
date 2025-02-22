687daa04ea4d2f342e153aab647c67360cfa5e9a
sivasakthi03-git <murugansivasakthi22@gmail.com>
2025-02-22 19:42:52 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,11 +5,11 @@ with open('README.md', 'r', encoding='utf-8') as f:
     long_description = f.read()     
    
 
-__version__ = "0.0.4"
-REPO_NAME = "mongodbconnectorpkg"
-PKG_NAME= "databaseautomation"
-AUTHOR_USER_NAME = "sunnysavita10"
-AUTHOR_EMAIL = "sunny.savita@ineuron.ai"
+__version__ = "0.0.3"
+REPO_NAME = "DBconnector"
+PKG_NAME= "mongo-db-automation"
+AUTHOR_USER_NAME = "sivasakthi"
+AUTHOR_EMAIL = "murugansivasakthi22@gmail.com"
 
 setup(
     name=PKG_NAME,
